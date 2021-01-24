e851e49e07f396b1153b52bc6d678aea70a5b8af
HardoScherz <hardo.scherz@gmail.com>
2021-01-24 17:38:17 +0100

    test

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+import re
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
 import pandas as pd
-import re
+import plotly.graph_objs as go
 import requests
-
 
 """ Daten holen
 """
